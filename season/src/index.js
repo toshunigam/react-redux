5ac38e52882db026e3b3b40aa8057348da3f6e86
toshunigam <toshuchandra@gmail.com>
2021-10-05 19:48:40 +0530

    learning into class based components

--- season/src/index.js
+++ season/src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {lat:null,long:null,errorMessage:''}
+        window.navigator.geolocation.getCurrentPosition(position=>{
+            this.setState({
+                lat:position.coords.latitude,
+                long:position.coords.longitude
+            })
+        },err=>{
+            this.setState({
+                errorMessage:err.message
+            })
+        })
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+    // This render() method must be invoked while working into react class based component
+    render(){
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if(!this.state.errorMessage && this.state.lat){
+            return <div>Latitude: {this.state.lat} , Longitude: {this.state.long}</div>
+        }
+
+        return <div>Loading...</div>
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
