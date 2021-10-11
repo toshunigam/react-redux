4225c0f2959c1d53f0d01c8ed03befbec0ef05c4
toshunigam <toshuchandra@gmail.com>
2021-10-11 15:51:38 +0530

    learing hooks in react

---
+++ widgets/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Accordion from './component/Accordion'
+
+const items = [
+    {
+        title:"what is react?",
+        content:"React is javascript framework introduced by Facebook."
+    },
+    {
+        title:"what is javascript?",
+        content:"Javascript is a language which runs on client side browser without using any dependancy."
+    },
+    {
+        title:"What is nodejs?",
+        content:"Nodejs is a server side language which is javascript library run and execute javascript code on server."
+    },
+    {
+        title:"what is java",
+        content:"Java is object oriented programming language."
+    }
+]
+const App = ()=>{
+    return (
+        <Accordion items={items} />
+    )
+    
+}
+
+export default App
