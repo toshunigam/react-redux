500dbac143c697eb8a441265142481d48788e404
toshunigam <toshuchandra@gmail.com>
2021-10-14 13:29:03 +0530

    working into axios and useEffect feature

--- widgets/src/component/Search.js
+++ widgets/src/component/Search.js
@@ -1,7 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
 
 const Search = ()=>{
-    const [term,setTerm] = useState('')
+    const [term,setTerm] = useState('programming')
+    const [debounceTerm, setDebounceTerm] = useState(term)
+    const [results,setResults] = useState([])
+    console.log('i run every render')
+
+    useEffect(()=>{
+        const timerId = setTimeout(()=>{
+            setDebounceTerm(term)
+        },1000)
+
+        return ()=>{
+            clearInterval(timerId)
+        }
+    },[term])
+
+
+    useEffect(()=>{
+        const search = async ()=>{
+            const {data} = await axios.get(`https://en.wikipedia.org/w/api.php`,{
+                params:{
+                    action:'query',
+                    list:'search',
+                    format:'json',
+                    origin:'*',
+                    srsearch:debounceTerm
+                }
+            })
+
+            setResults(data.query.search)
+        }
+
+            search()
+
+    },[debounceTerm])
+    const resultRenders = results.map((result,i)=>{
+        return (
+            <div className="item" key={i}>
+                <div className="right floated content">
+                    <a className="ui button" href={`https://en.wikipedia.org?currid=${result.pageid}`}>Go</a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{__html:result.snippet}}></span>
+                </div>
+            </div>
+        )
+    })
     return (
         <div>
             <div className="ui form">
@@ -9,6 +58,9 @@ const Search = ()=>{
                     <label>Search</label>
                     <input className="input" value={term} onChange={e=>setTerm(e.target.value)} />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {resultRenders}
             </div>
         </div>
     )
