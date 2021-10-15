5b3dfb01c4a471cfe62ae42044954845b62a020d
toshunigam <toshuchandra@gmail.com>
2021-10-15 11:15:53 +0530

    working into select dropdown

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import Accordion from './component/Accordion'
-import Search from './component/Search'
+import React, { useState } from 'react'
+// import Accordion from './component/Accordion'
+import Dropdown from './component/Dropdown'
+// import Search from './component/Search'
 
 const items = [
     {
@@ -20,12 +21,32 @@ const items = [
         content:"Java is object oriented programming language."
     }
 ]
+const options = [
+    {
+        label:'The color of red',
+        value:'red'
+    },
+    {
+        label:'The color of green',
+        value:'green'
+    },
+    {
+        label:'A shade of blue',
+        value:'blue'
+    },
+    {
+        label:'A shade of orange',
+        value:'orange'
+    }
+]
 const App = ()=>{
+    const [selected, setSelected] = useState(options[0])
     return (
         <div>
 
             {/* <Accordion items={items} /> */}
-            <Search />
+            {/* <Search /> */}
+            <Dropdown options={options} selected={selected} onSelectionChange={setSelected} />
         </div>
     )
     
--- widgets/src/component/Dropdown.js
+++ widgets/src/component/Dropdown.js
@@ -1,15 +1,54 @@
 import React from "react";
 
 const Dropdown = (props)=>{
+    const renderOptions = props.options.map((option)=>{
+        return(
+            <div 
+                className="item" 
+                key={option.value}
+                onClick={()=>props.onSelectionChange(option)}
+            >
+                {option.label}
+            </div>
+        )
+    })
     return(
-        <label>{props.label}</label>
+        <div className="ui form">
+            <div className="field">
+                <label>{props.title}</label>
+                <div className="ui selection dropdown visible active">
+                    <i className="dropdown icon"></i>
+                    <div className="text">{props.selected.label}</div>
+                    <div className="menu visible transition">
+                        {renderOptions}
+                    </div>
+                </div>
+            </div>
+        </div>
         
     )
 }
 
 Dropdown.defaultProps = {
-    options:[],
-    label:'',
-    defaultValue:'select one'
+    title:'Select a color',
+    selected:{
+        value:'',
+        label:'Select One'
+    },
+    options:[
+        {
+            label:'The color of red',
+            value:'red'
+        },
+        {
+            label:'The color of green',
+            value:'green'
+        },
+        {
+            label:'A shade of blue',
+            value:'blue'
+        }
+    ],
+    
 }
 export default Dropdown
