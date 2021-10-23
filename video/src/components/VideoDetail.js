82c92cf0e04d6266bd0a715c56d7b68a9b85e881
toshunigam <toshuchandra@gmail.com>
2021-10-23 11:31:50 +0530

    working assessment on youtube video search and list

--- video/src/components/VideoDetail.js
+++ video/src/components/VideoDetail.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 const VideoDetail = ({ video })=>{
+    if(!video){
+        return <div>Loading...</div>
+    }
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
     return(
         <div className="">
-            {video.snippet.title}
+            <div className="ui embed">
+                <iframe src={videoSrc} />
+            </div>
+
+            <div className="ui segment">
+                <h4 className="ui header"> {video.snippet.title} </h4>
+                <p>{video.snippet.description}</p>
+            </div>
         </div>
     )
 }
