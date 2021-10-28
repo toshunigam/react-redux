d0a80e8f608a5127c4a14cbebb83f4171ea0623a
toshunigam <toshuchandra@gmail.com>
2021-10-28 15:17:18 +0530

    working into redux actions and reducers

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,7 @@
+const songsReducers = ()=>{
+    return [
+        {title:'Dua song',duration:'4.05'},
+        {title:'Pyar hone laga hai',duration:'4.05'},
+        {title:'ho jata hai pyar na jane koi',duration:'4.05'}
+    ]
+}
