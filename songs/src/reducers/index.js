cfbcf297b2ac5e0f44b833884d7d992917a27a64
toshunigam <toshuchandra@gmail.com>
2021-10-28 16:08:34 +0530

    connected store and reducers into providers

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,7 +1,21 @@
+import { combineReducers } from 'redux'
+
 const songsReducers = ()=>{
     return [
-        {title:'Dua song',duration:'4.05'},
-        {title:'Pyar hone laga hai',duration:'4.05'},
+        {title:'Dua song',duration:'2.05'},
+        {title:'Pyar hone laga hai',duration:'3.05'},
         {title:'ho jata hai pyar na jane koi',duration:'4.05'}
     ]
 }
+
+const selectedSongReducer = (selectedSong=null, action)=>{
+    if(action.type === 'SONG_SELECTED'){
+        return action.payload
+    }
+    return selectedSong
+}
+
+export default combineReducers({
+    songs:songsReducers,
+    selectedSong:selectedSongReducer
+})
