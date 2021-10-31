c9eccf6f08b29af4bcf8efcae3325540f299a26e
toshunigam <toshuchandra@gmail.com>
2021-10-31 15:15:18 +0530

    working into actions in redux

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
+import connect from 'react-redux'
+
+import { fetchPosts } from '../actions'
 
 class PostList extends Component{
+    componentDidMount(){
+        this.props.fetchPosts()
+    }
     render(){
         return(
             <div>Post List</div>
@@ -8,4 +14,4 @@ class PostList extends Component{
     }
 }
 
-export default PostList
+export default connect(null,{ fetchPosts })(PostList)
