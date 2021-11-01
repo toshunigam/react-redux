8660d1d889de38b3b63ce4e882544761bee0f03f
toshunigam <toshuchandra@gmail.com>
2021-11-01 13:03:04 +0530

    working into redux think middleware

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,9 @@
-export const fetchPosts = ()=>{
+import jsonPlaceholder from "../apis/jsonPlaceholder"
+
+export const fetchPosts = async ()=>{
+    const response = await jsonPlaceholder.get('/posts')
     return{
-        type:'FETCH_POST'
+        type:'FETCH_POST',
+        payload:response
     }
 }
