557acc57e71c4e3729020bd534ba72076bfa0f72
toshunigam <toshuchandra@gmail.com>
2021-11-05 22:45:45 +0530

    corrected map function issue

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -7,15 +7,33 @@ class PostList extends Component{
     componentDidMount(){
         this.props.fetchPosts()
     }
+
+    renderList(){
+        return this.props.posts!==undefined?this.props.posts.map((post)=>{
+            return(
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user" />
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                </div>
+            );
+        }):null
+    }
+
     render(){
         console.log(this.props.posts)
         return(
-            <div>Post List</div>
+            <div className="ui relaxed divided list">{this.renderList()}</div>
         )
     }
 }
 
 const mapStateToProp = (state)=>{
-    return {posts:state.posts}
+    return {posts:state.posts.data}
 }
+
 export default connect(mapStateToProp,{ fetchPosts })(PostList)
