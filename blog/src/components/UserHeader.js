4459962699e8875276224afada13783866e29299
toshunigam <toshuchandra@gmail.com>
2021-11-06 11:13:19 +0530

    worked into user header using redux

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -8,15 +8,20 @@ class UserHeader extends Component{
     }
 
     render(){
-        console.log(this.props.user)
+        // console.log(this.props.users)
+        const user = this.props.users.find(user=> user.id===this.props.userId)
+        if(!user){
+            return null
+        }
+
         return(
-            <div>User Header{this.props.user.name}</div>
+            <div className="header">{user.name}</div>
         )
     }
 }
 
 const mapStateToProp = (state)=>{
-    return {user:state.user}
+    return {users:state.users}
 }
 
 export default connect(mapStateToProp,{ fetchUser })(UserHeader)
--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -4,5 +4,5 @@ import userReducer from './userReducer'
 
 export default combineReducers({
     posts:postReducer,
-    user:userReducer
+    users:userReducer
 })
