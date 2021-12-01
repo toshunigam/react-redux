93686746f5c44ec44de578fd0ff13eb3837966ad
toshunigam <toshuchandra@gmail.com>
2021-12-01 15:13:52 +0530

    working into react context

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,9 +1,28 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
+import UserCreate from "./UserCreate";
 
 class App extends React.Component{
+    state = {language:'english'}
+    onLanguageChange = (language)=>{
+        this.setState({
+            language
+        })
+    }
+
     render(){
         return(
-            <div className="ui container">App</div>
+            <div className="ui container">
+                App
+                <div>
+                    Select a language
+                    <i className="flag us" onClick={()=>this.onLanguageChange('english')} />
+                    <i className="flag in" onClick={()=>this.onLanguageChange('hindi')} />
+                </div>
+                <LanguageContext.Provider value={this.state.language}>
+                <UserCreate />
+                </LanguageContext.Provider>
+            </div>
         )
     }
 }
